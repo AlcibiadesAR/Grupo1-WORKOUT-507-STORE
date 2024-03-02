403fe0b6e7762668f67d4c89c566558c344f01e8
AlcibiadesAR <alcibiadesacosta99@gmail.com>
2024-03-02 13:34:12 -0600

    arreglo del header

--- Public/js/index.js
+++ Public/js/index.js
@@ -1,1 +1,12 @@
-console.log("hola mundo");
+document.addEventListener('DOMContentLoaded', function () {
+    let boton = document.querySelectorAll('.button-delete');
+    let articulo = document.querySelectorAll('.description-article');
+
+    for ( let botones of boton){
+        botones.addEventListener('click', function(){
+           articulo.style.display = 'none'
+        })
+    }
+   
+});
+
