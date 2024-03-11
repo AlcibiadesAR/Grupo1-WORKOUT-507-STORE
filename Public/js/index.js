88d8c9d0e7ba1aa47c766061dd57c69ad4ba20d7
AlcibiadesAR <alcibiadesacosta99@gmail.com>
2024-03-11 08:19:32 -0500

    fondo del body

--- Public/js/index.js
+++ Public/js/index.js
@@ -20,7 +20,7 @@ icon.addEventListener("click", function () {
 //modo lightMode
 function lightMode(body, header, footer, icon) {
   body.style.backgroundImage =
-    "url(https://png.pngtree.com/background/20230519/original/pngtree-an-old-gym-setting-with-dumbbells-picture-image_2667256.jpg)";
+    "url(https://img.freepik.com/fotos-premium/fondo-equipos-fitness-gimnasio-entrenamiento-espacio-copia-texto_916191-122007.jpg)";
   body.style.backgroundRepeat = "no repeat";
   body.style.backgroundSize = "cover";
   header.style.backgroundColor = "#00e1ff";
@@ -32,8 +32,8 @@ function lightMode(body, header, footer, icon) {
 
 //modo darkMode
 function darkMode(body, header, footer, icon, h1, shoppingIcon) {
-  body.style.backgroundImage = "none";
-  body.style.backgroundColor = "#000";
+  body.style.backgroundImage = "url(https://png.pngtree.com/thumb_back/fw800/background/20230630/pngtree-dark-fitness-room-with-training-equipment-and-black-dumbbells-on-the-image_3698810.jpg)";
+  body.style.backgroundColor = "none";
   header.style.backgroundColor = "#2C2F35";
   footer.style.backgroundColor = "#2C2F35";
   footer.style.borderTop = "2px solid #a9a9a9";
