b189fd2871408dcafdc8e8623c53ff24a58cf821
JonathanAlexander507 <sting12520@gmail.com>
2024-03-12 11:37:32 -0500

    haciendo validacion del registro

---
+++ Public/js/validación.js
@@ -0,0 +1,23 @@
+let formulario = document.querySelector('form');
+let nombre = document.querySelector('#Nombre')
+let apellido = document.querySelector('#Apellido')
+let correo = document.querySelector('#Correo')
+let contraseña = document.querySelector('#Contraseña')
+let erroresUl = document.querySelector('#errores')
+
+formulario.addEventListener("submit",function(e){
+    e.preventDefault()
+    let errores = []
+    erroresUl.innerHTML = ''
+    if (nombre.value == '') errores.push('El campo de nombre no debe quedar vacio')
+    if (apellido.value == '') errores.push('El campo apellido no debe estar vacio')
+    if (email.value == '') errores.push('el campo no puede estar vacio')
+    if (contraseña.value == '') errores.push('el campo no puede quedar vacio')
+    if (errores.length > 0) {
+        for (const item of errores){
+        erroresUl.innerHTML += `<li>${item}</li>`
+        }
+    } else {
+        alert('formulario enviado')
+    }
+})
