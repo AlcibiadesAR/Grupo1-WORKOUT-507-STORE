742b289c9358d247b2a5229cf608a9f290b8d967
JonathanAlexander507 <sting12520@gmail.com>
2024-03-13 11:45:24 -0500

    modificaciones del home

--- Public/js/validación.js
+++ Public/js/validación.js
@@ -7,11 +7,12 @@ let erroresUl = document.querySelector('#errores')
 
 formulario.addEventListener("submit",function(e){
     e.preventDefault()
+    console.log("hola");
     let errores = []
     erroresUl.innerHTML = ''
     if (nombre.value == '') errores.push('El campo de nombre no debe quedar vacio')
     if (apellido.value == '') errores.push('El campo apellido no debe estar vacio')
-    if (email.value == '') errores.push('el campo no puede estar vacio')
+    if (correo.value == '') errores.push('el campo no puede estar vacio')
     if (contraseña.value == '') errores.push('el campo no puede quedar vacio')
     if (errores.length > 0) {
         for (const item of errores){
