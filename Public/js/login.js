80f1421dcd326c7d0418e2c96c94aff2d04218b4
AlcibiadesAR <alcibiadesacosta99@gmail.com>
2024-03-14 12:01:07 -0500

    arreglos de login

--- Public/js/login.js
+++ Public/js/login.js
@@ -1,6 +1,39 @@
 /*=======login validacion=======*/
 let Email = document.querySelector('#emailValidar');
 let Password = document.querySelector('#passwordValidar');
-let emailError = document.querySelector('#emailError');
-let passWordError =document.querySelector('#passwordError')
 let form = document.querySelector('#formulario');
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+    if (Email.value.length == 0) {
+            validacion(Email);
+    } else {
+        alert('Formulario enviado con exito')
+    }
+});
+
+Email.addEventListener("focus", function () {
+    validacion(Email);
+  });
+
+  Password.addEventListener("input", function () {
+    validacion(Password);
+  });
+  
+function validacion(Email) {
+  let LoginError = document.querySelector("#errorEmail");
+  if (Email.value.length == 0)
+    LoginError.innerHTML = "Debe ingresar su correo electronico";
+  else {
+    LoginError.innerHTML = "";
+  }
+}
+
+function validacion(Password) {
+    let LoginError2 = document.querySelector("#errorPassword");
+    if (Password.value.length == 0)
+      LoginError2.innerHTML = "Debe ingresar su contraseña";
+    else {
+      LoginError2.innerHTML = "";
+    }
+  }
